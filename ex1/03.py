68292395e815d90ef8ba8cc2086ebb37eba0a308
EAGLE <eagle@EAGLEnoMacBook-Pro.local>
2019-04-12 22:01:40 +0900

    add ex1

---
+++ ex1/03.py
@@ -0,0 +1,14 @@
+a = 10
+b = 5
+c = 100
+d = a + b
+e = a * b
+f = a / b
+g = a // b
+h = a ** b
+
+print('a+b=%d' % d)
+print('a*b=%d' % e)
+print('a/b=%d' % f)
+print('a//b=%d' % g)
+print('a**b=%d' % h)
